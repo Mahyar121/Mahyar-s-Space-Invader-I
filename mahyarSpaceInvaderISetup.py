6d768fa15f2156f9bdb4e93e882e04237f37065f
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-11-02 01:42:47 -0700

    Update mahyarSpaceInvaderISetup.py

--- mahyarSpaceInvaderISetup.py
+++ mahyarSpaceInvaderISetup.py
@@ -5,7 +5,7 @@ executables = [cx_Freeze.Executable("mahyarSpaceInvaderI.py", icon="player.ico")
 cx_Freeze.setup(
     name = "Mahyar's Space Invader I",
     options={"build_exe":{"packages":["pygame"],
-                          "include_files":["e1_0.png","e1_1.png","e2_0.png","e2_1.png","e3_0.png","e3_1.png","game_font.ttf","player.png"]}},
+                          "include_files":["e1_0.png","e1_1.png","e2_0.png","e2_1.png","e3_0.png","e3_1.png","rocket.png","rocket2.png","game_font.ttf","player.png"]}},
 
     description ="Mahyar's Space Invader I Game",
     executables = executables
