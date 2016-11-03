d23362837bb05858cfcc0ef79401f19b9e4fd702
Mahyar Babaie <mahyarhajibabaie@gmail.com>
2016-11-02 18:03:14 -0700

    Add files via upload

--- MahyarSpaceInvaderI.py
+++ MahyarSpaceInvaderI.py
@@ -375,16 +375,26 @@ class MahyarSpaceInvaderI:
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         #Quit button
-        if 275 + 260 > mouse[0] > 275 and 455 + 50 > mouse[1] > 455:
-            self.buttonBorder(self.white, 270,450,270,60)
-            self.buttonBorder(self.brightGreen,275,455,260,50)
+        if 275 + 260 > mouse[0] > 275 and 505 + 50 > mouse[1] > 505:
+            self.buttonBorder(self.white, 270,500,270,60)
+            self.buttonBorder(self.brightGreen,275,505,260,50)
             if click[0] == 1:
                 pygame.quit()
                 quit()
         else:
-            self.buttonBorder(self.white,270,450,270,60)
-            self.buttonBorder(self.green,275,455,260,50)
-        self.textButton("Quit",275,455,260,50)
+            self.buttonBorder(self.white,270,500,270,60)
+            self.buttonBorder(self.green,275,505,260,50)
+        self.textButton("Quit",275,505,260,50)
+        # HighScores button
+        if 275 + 260 > mouse[0] > 275 and 405 + 50 > mouse[1] > 405:
+            self.buttonBorder(self.white, 270, 400, 270, 60)
+            self.buttonBorder(self.brightGreen, 275, 405, 260, 50)
+            if click[0] == 1:
+                self.controlsPage()
+        else:
+            self.buttonBorder(self.white, 270, 400, 270, 60)
+            self.buttonBorder(self.green, 275, 405, 260, 50)
+        self.textButton("High Scores", 275, 405, 260, 50)
         #Controls button
         if 275 + 260 > mouse[0] > 275 and 305 + 50 > mouse[1] > 305:
             self.buttonBorder(self.white, 270, 300, 270, 60)
@@ -395,16 +405,26 @@ class MahyarSpaceInvaderI:
             self.buttonBorder(self.white, 270, 300, 270, 60)
             self.buttonBorder(self.green,275,305,260,50)
         self.textButton("Controls",275,305,260,50)
+        #Tutorial button
+        if 275 + 260 > mouse[0] > 275 and 205 + 50 > mouse[1] > 205:
+            self.buttonBorder(self.white, 270, 200, 270, 60)
+            self.buttonBorder(self.brightGreen, 275, 205, 260, 50)
+            if click[0] == 1:
+                self.controlsPage()
+        else:
+            self.buttonBorder(self.white, 270, 200, 270, 60)
+            self.buttonBorder(self.green, 275, 205, 260, 50)
+        self.textButton("Tutorial", 275, 205, 260, 50)
         #Play button
-        if 275 + 260 > mouse[0] > 275 and 155 + 50 > mouse[1] > 155:
-            self.buttonBorder(self.white,270,150,270,60)
-            self.buttonBorder(self.brightGreen, 275,155,260,50)
+        if 275 + 260 > mouse[0] > 275 and 105 + 50 > mouse[1] > 105:
+            self.buttonBorder(self.white,270,100,270,60)
+            self.buttonBorder(self.brightGreen, 275,105,260,50)
             if click[0] == 1:
                 self.run()
         else:
-            self.buttonBorder(self.white, 270, 150, 270, 60)
-            self.buttonBorder(self.green, 275, 155, 260, 50)
-        self.textButton("Play",275,155,260,50)
+            self.buttonBorder(self.white, 270, 100, 270, 60)
+            self.buttonBorder(self.green, 275, 105, 260, 50)
+        self.textButton("Play",275,105,260,50)
 
     def game_intro(self):
         intro = True
